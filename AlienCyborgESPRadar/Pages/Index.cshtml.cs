53ce5bf16a9adc64e7a9708ea645f3e250f3730a
Gabriel <gvogt@asu.edu>
2025-12-19 21:50:00 -0700

    Add project files.

---
+++ AlienCyborgESPRadar/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AlienCyborgESPRadar.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
