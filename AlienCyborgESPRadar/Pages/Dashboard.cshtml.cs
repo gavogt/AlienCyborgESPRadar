b772a84aad5b7ad7b011d47b2622ed6a45ffb696
Gabriel <gvogt@asu.edu>
2025-12-21 13:10:52 -0700

    added a splash page, moved index to dashboard, redirect from splash index to dashboard

---
+++ AlienCyborgESPRadar/Pages/Dashboard.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AlienCyborgESPRadar.Pages
+{
+    public class Dashboard : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
