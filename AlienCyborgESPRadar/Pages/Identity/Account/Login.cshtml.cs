fcec815dd4ee1fd25191a4f9d039108ff9df500a
Gabriel <gvogt@asu.edu>
2025-12-22 12:49:50 -0700

    add login page and refresh images with new images

--- AlienCyborgESPRadar/Pages/Identity/Account/Login.cshtml.cs
+++ AlienCyborgESPRadar/Pages/Identity/Account/Login.cshtml.cs
@@ -1,6 +1,28 @@
-﻿namespace AlienCyborgESPRadar.Pages.Identity.Account
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+namespace AlienCyborgESPRadar.Pages.Identity.Account
 {
-    public class Login
+    public class LoginModel : PageModel
     {
+        [BindProperty]
+        public InputModel Input { get; set; } = new();
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; } = string.Empty;
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; } = string.Empty;
+
+            public bool RememberMe
+            {
+                get; set;
+
+            }
+        }
     }
 }
