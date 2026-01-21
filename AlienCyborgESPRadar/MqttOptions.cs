b78dbbb7826574d07e7fc93f7f55e83dd8f02f12
Gabriel <gvogt@asu.edu>
2026-01-20 21:07:19 -0700

    removed redundant mqttoptions

--- AlienCyborgESPRadar/MqttOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AlienCyborgESPRadar
-{
-    public sealed class MqttOptions
-    {
-        public string Host { get; set; } = "192.168.1.197";
-        public int Port { get; set; } = 1883;
-        public string Topic { get; set; } = "#";
-    }
-}
--- AlienCyborgESPRadar/Services/MqttOptions.cs
+++ AlienCyborgESPRadar/Services/MqttOptions.cs
@@ -1,6 +1,6 @@
 ﻿public sealed class MqttOptions
 {
-    public string Host { get; set;  } = "localhost";
+    public string Host { get; set;  } = "192.168.1.197";
     public int Port { get; set; } = 1883;
     public string Topic { get; set; } = "/#";
     public string? Username { get; set; }   
