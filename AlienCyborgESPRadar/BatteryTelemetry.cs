d1683cc4185211625adaa23bd0c641d6148eee94
Gabriel <gvogt@asu.edu>
2026-01-25 17:34:03 -0700

    add BatteryTelemetry class for MAX17048 chip data

---
+++ AlienCyborgESPRadar/BatteryTelemetry.cs
@@ -0,0 +1,12 @@
+﻿namespace AlienCyborgESPRadar
+{
+    public sealed class BatteryTelemetry
+    {
+
+        public bool Ok { get; init; }
+        public double VoltageV { get; init; }
+        public double StateOfChargePct { get; init; }
+        public double? RatePctPerHour { get; init; }
+        public byte? ChipId { get; init; }
+    }
+}
